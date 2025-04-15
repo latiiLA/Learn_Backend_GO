1f50f42fe78d10cafd314e3ff4ef3146e9d6afa7
Lata <amelatbay@gmail.com>
2025-04-15 20:14:59 +0300

    task controllers for adding new tasks, updating tasking, fetching all tasks, fetching specific tasks, and removing task implemented

--- Task5/task_manager/controllers/task_controller.go
+++ Task5/task_manager/controllers/task_controller.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"net/http"
-	"task_manager/db"
+	"task_manager/data"
 	"task_manager/models"
 
 	"github.com/gin-gonic/gin"
@@ -10,56 +10,91 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func GetTasks(collections *db.DBCollections) func (ctx *gin.Context){
-	return func (ctx *gin.Context) {
-
-		cursor, err := collections.Tasks.Find(ctx, bson.M{})
-		if err != nil{
+func GetTasks(taskCollection *mongo.Collection) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tasks, err := data.GetTasks(taskCollection)
+		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{
 				"error": "Failed to fetch tasks from database",
 			})
 			return
-		}
-		defer cursor.Close(ctx)
-
-		var tasks []models.Task
-		if err = cursor.All(ctx, &tasks); err != nil{
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode task data",})
-			return
-		}
-
-		if tasks == nil{
-			tasks = []models.Task{}
 		}
 
 		ctx.JSON(http.StatusOK, gin.H{"tasks": tasks})
 	}
 }
 
-func GetTask(collections *db.DBCollections) gin.HandlerFunc{
-	return func (ctx *gin.Context){
+
+func GetTask(taskCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		// Get the task ID from URL parameters
 		taskID := ctx.Param("id")
 
-		var task models.Task
-		err := collections.Tasks.FindOne(ctx, bson.M{"id": taskID}).Decode(&task)
+		// Call the service to fetch the task
+		task, err := data.GetTask(taskCollection, taskID)
 		if err != nil{
 			if err == mongo.ErrNoDocuments{
 				ctx.JSON(http.StatusNotFound, gin.H{
 					"error": "Task not found",
 				})
-				return
+			}else{
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error": "Failed to fetch task",
+				})
 			}
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch task",})
 			return
 		}
 
-		ctx.JSON(http.StatusOK, gin.H{"task" : task})
+		// Send the task and part of response
+		ctx.JSON(http.StatusOK, gin.H{
+			"task": task,
+		})
 	}
 }
 
-func UpdateTask(collections *db.DBCollections) gin.HandlerFunc{
-	return func (ctx *gin.Context){
-		taskID := ctx.Param("id") // This is a string-based ID
+func AddTask(taskCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		var newTask models.Task
+		if err := ctx.ShouldBindJSON(&newTask); err != nil{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+		if newTask.ID == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "ID is required"})
+			return
+		}
+
+		if newTask.Title == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Title is required"})
+			return
+		}
+
+		if newTask.Description == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Description is required"})
+			return
+		}
+
+		if newTask.Status == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Status is required"})
+			return
+		}
+
+		// Call the service to add the task
+		InsertedID, err := data.AddTask(taskCollection, newTask)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Failed to add task",
+			})
+		}
+
+		ctx.JSON(http.StatusAccepted, gin.H{"message": "Task created successfully", "task": newTask, "id": InsertedID})
+	}
+}
+
+func UpdateTask(taskCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		taskID := ctx.Param("id")
 		if taskID == "" {
             ctx.JSON(http.StatusBadRequest, gin.H{"error": "Task ID is required"})
             return
@@ -93,92 +128,56 @@ func UpdateTask(collections *db.DBCollections) gin.HandlerFunc{
             return
         }
 
-		result, err := collections.Tasks.UpdateOne(
-            ctx,
-            bson.M{"id": taskID},
-            bson.M{"$set": update},
-        )
+		// Call the service to update the task
+		MatchedCount, ModifiedCount, err := data.UpdateTask(taskCollection, taskID, update)
+		if err != nil{
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error": "Failed to update task",
+			})
+			return
+		}
 
-		if err != nil {
-            ctx.JSON(http.StatusInternalServerError, gin.H{
-                "error": "Failed to update task",
-                "details": err.Error(),
-            })
-            return
-        }
-
-        if result.MatchedCount == 0 {
-            ctx.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
-            return
-        }
-
-        ctx.JSON(http.StatusOK, gin.H{
-            "message": "Task updated successfully",
-            "updatedCount": result.ModifiedCount,
-        })
+	
+		if MatchedCount == 0 {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+			return
+		}
+		
+		// Send response
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Task updated successfully",
+			"updatedCount": ModifiedCount,
+		})
 	}
 }
 
-func RemoveTask(collections *db.DBCollections) gin.HandlerFunc{
-	return func (ctx *gin.Context){
+func RemoveTask(taskCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
 		taskID := ctx.Param("id")
 		if taskID == ""{
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Task ID is required"})
 			return
 		}
 
-		result, err := collections.Tasks.DeleteOne(ctx, bson.M{"id": taskID})
-		if err != nil{
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete task"}) 
-			return
-		}
-
-		if result.DeletedCount == 0{
-			ctx.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
-			return
-		}
-
-		ctx.JSON(http.StatusOK, gin.H{"message": "Task deleted successfully", "deletedCount" : result.DeletedCount})
-	}
-}
-
-func AddTask(collections *db.DBCollections) gin.HandlerFunc{
-	return func (ctx *gin.Context){
-		var newTask models.Task
-		if err := ctx.ShouldBindJSON(&newTask); err != nil{
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
-			return
-		}
-
-		if newTask.ID == ""{
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "ID is required"})
-			return
-		}
-
-
-		if newTask.Title == ""{
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Title is required"})
-			return
-		}
-
-		if newTask.Description == ""{
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Description is required"})
-			return
-		}
-
-		if newTask.Status == ""{
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Status is required"})
-			return
-		}
-
-		result, err := collections.Tasks.InsertOne(ctx, newTask)
+		// Call the service to update the task
+		DeletedCount, err := data.RemoveTask(taskCollection, taskID)
+		print(DeletedCount, err)
 		if err != nil{
 			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"error": "Failed to create task",
+				"error": "Failed to remove task",
 			})
 			return
 		}
 
-		ctx.JSON(http.StatusAccepted, gin.H{"message": "Task created successfully", "task": newTask, "id": result.InsertedID})
+		if DeletedCount == 0 {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+			return
+		}
+
+		// Send response
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Task removed successfully",
+			"deletedCount": DeletedCount,
+		})
 	}
 }
