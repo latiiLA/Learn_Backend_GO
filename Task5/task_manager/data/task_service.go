98d2ac9378690fa492f2d7945617065787763397
Lata <amelatbay@gmail.com>
2025-04-15 20:15:20 +0300

    task services for adding new tasks, updating tasking, fetching all tasks, fetching specific tasks, and removing task implemented

--- Task5/task_manager/data/task_service.go
+++ Task5/task_manager/data/task_service.go
@@ -1,14 +1,76 @@
 package data
 
 import (
-	"time"
-
+	"context"
 	"task_manager/models"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// Mock data for tasks
-var Tasks = []models.Task{
-	{ID: "1", Title: "Task 1", Description: "First task", DueDate: time.Now(), Status: "Pending"},
-    {ID: "2", Title: "Task 2", Description: "Second task", DueDate: time.Now().AddDate(0, 0, 1), Status: "In Progress"},
-    {ID: "3", Title: "Task 3", Description: "Third task", DueDate: time.Now().AddDate(0, 0, 2), Status: "Completed"},
+func GetTasks(collection *mongo.Collection) ([]models.Task, error){
+
+		cursor, err := collection.Find(context.TODO(), bson.M{})
+		if err != nil{
+			return nil, err
+		}
+		defer cursor.Close(context.TODO())
+
+		var tasks []models.Task
+		if err = cursor.All(context.TODO(), &tasks); err != nil{
+			return nil, err
+		}
+
+		if tasks == nil{
+			tasks = []models.Task{}
+		}
+
+		return tasks, nil
+	
+}
+
+func GetTask(collection *mongo.Collection, taskID string) (models.Task, error){
+	var task models.Task
+	err := collection.FindOne(context.TODO(), bson.M{"id": taskID}).Decode(&task)
+	if err != nil{
+		if err == mongo.ErrNoDocuments{
+			return task, err
+		}
+	}
+
+	return task, nil
+}
+
+func UpdateTask(collection *mongo.Collection, taskID string, update bson.M) (int64, int64, error){
+	result, err := collection.UpdateOne(
+		context.TODO(),
+		bson.M{"id": taskID},
+		bson.M{"$set": update},
+	)
+
+	if err != nil{
+		return 0, 0, err
+	}
+	return result.MatchedCount, result.ModifiedCount, nil
+	
+}
+
+
+func RemoveTask(collection *mongo.Collection, taskID string) (int64, error){
+	
+	result, err := collection.DeleteOne(context.TODO(), bson.M{"id": taskID})
+	if err != nil{
+		return 0, err
+	}
+
+	return result.DeletedCount, nil
+
+}
+
+func AddTask(collection *mongo.Collection, newTask models.Task) (any, error) {
+	result, err := collection.InsertOne(context.TODO(), newTask)
+	if err != nil {
+		return nil, err
+	}
+	return result.InsertedID, nil
 }
