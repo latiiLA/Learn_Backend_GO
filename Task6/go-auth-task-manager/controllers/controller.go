7e7caa254d90620bed8c311eaba491bc0d8eeea1
Lata <amelatbay@gmail.com>
2025-04-16 22:34:01 +0300

    task and user controller added

---
+++ Task6/go-auth-task-manager/controllers/controller.go
@@ -0,0 +1,227 @@
+package controllers
+
+import (
+	"go-auth-task-manager/data"
+	"go-auth-task-manager/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func GetTasks(taskCollection *mongo.Collection) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tasks, err := data.GetTasks(taskCollection)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Failed to fetch tasks from database",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"tasks": tasks})
+	}
+}
+
+
+func GetTask(taskCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		// Get the task ID from URL parameters
+		taskID := ctx.Param("id")
+
+		// Call the service to fetch the task
+		task, err := data.GetTask(taskCollection, taskID)
+		if err != nil{
+			if err == mongo.ErrNoDocuments{
+				ctx.JSON(http.StatusNotFound, gin.H{
+					"error": "Task not found",
+				})
+			}else{
+				ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error": "Failed to fetch task",
+				})
+			}
+			return
+		}
+
+		// Send the task and part of response
+		ctx.JSON(http.StatusOK, gin.H{
+			"task": task,
+		})
+	}
+}
+
+func AddTask(taskCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		var newTask models.Task
+		if err := ctx.ShouldBindJSON(&newTask); err != nil{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+		if newTask.ID == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "ID is required"})
+			return
+		}
+
+		if newTask.Title == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Title is required"})
+			return
+		}
+
+		if newTask.Description == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Description is required"})
+			return
+		}
+
+		if newTask.Status == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Status is required"})
+			return
+		}
+
+		// Call the service to add the task
+		InsertedID, err := data.AddTask(taskCollection, newTask)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Failed to add task",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusAccepted, gin.H{"message": "Task created successfully", "task": newTask, "id": InsertedID})
+	}
+}
+
+func UpdateTask(taskCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		taskID := ctx.Param("id")
+		if taskID == "" {
+            ctx.JSON(http.StatusBadRequest, gin.H{"error": "Task ID is required"})
+            return
+        }
+
+		var updatedData models.Task
+        if err := ctx.ShouldBindJSON(&updatedData); err != nil {
+            ctx.JSON(http.StatusBadRequest, gin.H{
+                "error": "Invalid request body",
+                "details": err.Error(),
+            })
+            return
+        }
+
+        update := bson.M{}
+        if updatedData.Title != "" {
+            update["title"] = updatedData.Title
+        }
+        if updatedData.Description != "" {
+            update["description"] = updatedData.Description
+        }
+        if !updatedData.DueDate.IsZero() {
+            update["due_date"] = updatedData.DueDate
+        }
+        if updatedData.Status != "" {
+            update["status"] = updatedData.Status
+        }
+
+        if len(update) == 0 {
+            ctx.JSON(http.StatusBadRequest, gin.H{"error": "No fields to update"})
+            return
+        }
+
+		// Call the service to update the task
+		MatchedCount, ModifiedCount, err := data.UpdateTask(taskCollection, taskID, update)
+		if err != nil{
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+					"error": "Failed to update task",
+			})
+			return
+		}
+
+	
+		if MatchedCount == 0 {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+			return
+		}
+		
+		// Send response
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Task updated successfully",
+			"updatedCount": ModifiedCount,
+		})
+	}
+}
+
+func RemoveTask(taskCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		taskID := ctx.Param("id")
+		if taskID == ""{
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Task ID is required"})
+			return
+		}
+
+		// Call the service to update the task
+		DeletedCount, err := data.RemoveTask(taskCollection, taskID)
+		print(DeletedCount, err)
+		if err != nil{
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Failed to remove task",
+			})
+			return
+		}
+
+		if DeletedCount == 0 {
+			ctx.JSON(http.StatusNotFound, gin.H{"error": "Task not found"})
+			return
+		}
+
+		// Send response
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "Task removed successfully",
+			"deletedCount": DeletedCount,
+		})
+	}
+}
+
+func Register(userCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		var user models.User
+		if err := ctx.ShouldBindJSON(&user); err != nil {
+			ctx.JSON(400, gin.H{"error": "Invalid request payload"})
+			return
+		}
+
+		// Call the user service to add the user
+		InsertedID, err := data.Register(userCollection, user)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Failed to register user",
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusAccepted, gin.H{"message": "User registered successfully", "user": user, "id": InsertedID})
+	}
+}
+
+func Login(userCollection *mongo.Collection) gin.HandlerFunc{
+	return func(ctx *gin.Context){
+		var user models.User
+		if err := ctx.ShouldBindJSON(&user); err != nil {
+			ctx.JSON(400, gin.H{"error": "Invalid request payload"})
+			return
+		}
+
+		// Call the user service to add the user
+		token, err := data.Login(userCollection, user)
+		
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{
+				"error": "Internal server error",
+			})
+			return
+		}
+
+		ctx.JSON(200, gin.H{"message": "User logged in successfull", "token": token})
+	} 
+}
