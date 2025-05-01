2e7c1b9bf93f0cbf876756f8dcd0a1e18073621d
Lata <amelatbay@gmail.com>
2025-05-01 15:17:37 +0300

    update task, fetch all task, deleted task, fetch task by task id repositories added

--- Task7/task_management_clean_architecture/Repositories/task_repositories.go
+++ Task7/task_management_clean_architecture/Repositories/task_repositories.go
@@ -51,3 +51,65 @@ func (tr *taskRepository) FetchByUserID(c context.Context, userID string)([]doma
 
 	return tasks, err
 }
+
+func (tr *taskRepository) FetchAll(c context.Context)([]domain.Task, error){
+	collection := tr.database.Collection(tr.collection)
+
+	var tasks []domain.Task
+
+	cursor, err := collection.Find(c, bson.M{})
+	if err != nil{
+		return nil, err
+	}
+	defer cursor.Close(c)
+	
+	if err := cursor.All(c, &tasks); err != nil{
+		return nil, err
+	}
+
+	return tasks, err
+}
+
+func (tr *taskRepository) UpdateByTaskID(c context.Context, taskID primitive.ObjectID, updatedData *domain.Task)(int64, int64, error){
+	collection := tr.database.Collection(tr.collection)
+
+	result, err := collection.UpdateOne(
+		c,
+		bson.M{"_id": taskID},
+		bson.M{"$set": updatedData},
+	)
+
+	if err != nil{
+		return 0, 0, err
+	}
+	return result.MatchedCount, result.ModifiedCount, nil
+}
+
+func (tr *taskRepository) DeleteByTaskID(c context.Context, taskID primitive.ObjectID)(int64, error){
+	collection := tr.database.Collection(tr.collection)
+
+	result, err := collection.DeleteOne(
+		c, 
+		bson.M{"_id": taskID},
+	)
+
+	if err != nil{
+		return 0, err
+	}
+
+	return result.DeletedCount, nil
+}
+
+func (tr *taskRepository) FetchByTaskID(c context.Context, taskID primitive.ObjectID)(domain.Task, error){
+	collection := tr.database.Collection(tr.collection)
+
+	var task domain.Task
+
+	err := collection.FindOne(c, bson.M{"_id": taskID}).Decode(&task)
+
+	if err != nil{
+		return domain.Task{}, err
+	}
+
+	return task, nil
+}
