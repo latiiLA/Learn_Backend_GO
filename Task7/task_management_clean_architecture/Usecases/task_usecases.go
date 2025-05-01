4feabd441a0458bf8bda47a9db0e9573483952f0
Lata <amelatbay@gmail.com>
2025-05-01 15:17:52 +0300

    update task, fetch all task, deleted task, fetch task by task id usecases added

--- Task7/task_management_clean_architecture/Usecases/task_usecases.go
+++ Task7/task_management_clean_architecture/Usecases/task_usecases.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	domain "task_management_clean_architecture/Domain"
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type taskUsecase struct {
@@ -28,4 +30,28 @@ func (tu *taskUsecase) GetTasksByUserID(c context.Context, userID string)([]doma
 	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
 	defer cancel()
 	return tu.taskRepository.FetchByUserID(ctx, userID)
+}
+
+func (tu *taskUsecase) GetAllTasks(c context.Context)([]domain.Task, error){
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.FetchAll(ctx)
+}
+
+func (tu *taskUsecase) UpdateTask(c context.Context, taskID primitive.ObjectID, updatedData *domain.Task)(int64, int64, error){
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.UpdateByTaskID(ctx, taskID, updatedData)
+}
+
+func (tu *taskUsecase) DeleteTask(c context.Context, taskID primitive.ObjectID)(int64, error){
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.DeleteByTaskID(ctx, taskID)
+}
+
+func (tu *taskUsecase) GetSpecificTask(c context.Context, taskID primitive.ObjectID)(domain.Task, error){
+	ctx, cancel := context.WithTimeout(c, tu.contextTimeout)
+	defer cancel()
+	return tu.taskRepository.FetchByTaskID(ctx, taskID)
 }
